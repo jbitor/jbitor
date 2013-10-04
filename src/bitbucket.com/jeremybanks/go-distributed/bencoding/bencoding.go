188fdf2b1bd4cb8a0c798c35f9b3cd7b600c133c
Jeremy Banks <jeremy@jeremybanks.ca>
2013-10-03 20:32:38 -0400

    Bencoding seeming to work on variably-typed data.

--- src/bitbucket.com/jeremybanks/go-distributed/bencoding/bencoding.go
+++ src/bitbucket.com/jeremybanks/go-distributed/bencoding/bencoding.go
@@ -4,7 +4,13 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"reflect"
 )
+
+/*
+	Provides a BValue type for encoding/decoding data as described here:
+	https://wiki.theory.org/BitTorrentSpecification#Bencoding
+*/
 
 type BValueType int
 
@@ -20,74 +26,180 @@ type BValue struct {
 	string_value string
 	int_value    int64
 	list_value   []BValue
-	map_value    map[string]BValue
+	dict_value   map[string]BValue
 }
 
-func (value BValue) String() string {
-	// Returns a JSON-like representation of this bencoding.Value.
-
-	switch value.t {
+func (bval BValue) WriteBencoded(buffer *bytes.Buffer) (err error) {
+	switch bval.t {
 	case STRING:
-		return value.string_value
+		buffer.WriteString(
+			fmt.Sprintf("%v:%v", len(bval.string_value), bval.string_value))
 
 	case INTEGER:
-		return fmt.Sprintf("%v", value.int_value)
+		buffer.WriteString(
+			fmt.Sprintf("i%ve", bval.int_value))
 
 	case LIST:
-		var buffer bytes.Buffer
-		buffer.WriteString("[")
-
-		for index, value := range value.list_value {
-			if index > 0 {
-				buffer.WriteString(", ")
+		buffer.WriteString("l")
+		for _, item := range bval.list_value {
+			var item_str string
+			item_str, err = item.Bencode()
+			if err != nil {
+				return
 			}
-
-			buffer.WriteString(value.String())
+			buffer.WriteString(item_str)
 		}
-
-		buffer.WriteString("]")
-		return buffer.String()
+		buffer.WriteString("e")
 
 	case DICTIONARY:
-		return "{ ... }"
+		for key, item := range bval.dict_value {
+			buffer.WriteString(
+				fmt.Sprintf("%v:%v", len(key), key))
+
+			var item_str string
+			item_str, err = item.Bencode()
+			if err != nil {
+				return
+			}
+			buffer.WriteString(item_str)
+		}
+
+	default:
+		err = errors.New(fmt.Sprintf("Illegal BValue.t: %v", bval.t))
 	}
 
-	return "<invalid .t for bencoding.Value>"
+	return
 }
 
-func NewBValue(data interface{}) (value *BValue, err error) {
-	// Uses reflection to convert an arbitrary object to a
-	// bencoding.Value if possible.
+func (bval BValue) Bencode() (str string, err error) {
+	var buffer bytes.Buffer
 
-	return nil, errors.New("Not implemented")
+	err = bval.WriteBencoded(&buffer)
+
+	if err == nil {
+		str = buffer.String()
+	}
+
+	return
 }
 
-func NewBValueString(val string) *BValue {
-	return &BValue{t: STRING, string_value: val}
+func Bdecode(string) (bval *BValue, err error) {
+	err = errors.New("Bdecode Not implemented")
+
+	return
 }
 
 func Bencode(data interface{}) (bencoded string, err error) {
 	val, err := NewBValue(data)
 
 	if err == nil {
-		bencoded = val.Bencode()
+		bencoded, err = val.Bencode()
 	}
 
 	return
 }
 
-func (value BValue) Bencode() string {
-	return ""
+func NewBValue(data interface{}) (bval *BValue, err error) {
+	// Uses reflection to convert an arbitrary object to a
+	// bencoding.Value if possible.
+
+	// http://blog.golang.org/laws-of-reflection
+
+	t := reflect.TypeOf(data)
+
+	switch t.Kind() {
+	case reflect.Int64:
+		bval = &(BValue{t: INTEGER, int_value: data.(int64)})
+	case reflect.String:
+		bval = &(BValue{t: STRING, string_value: data.(string)})
+	case reflect.Map:
+		bval = &(BValue{t: DICTIONARY, dict_value: make(map[string]BValue)})
+
+		for key, item := range data.(map[string]interface{}) {
+			var item_bval *BValue
+
+			item_bval, err = NewBValue(item)
+
+			if err != nil {
+				return
+			}
+
+			bval.dict_value[key] = *item_bval
+		}
+	case reflect.Array, reflect.Slice:
+		length := len(data.([]interface{}))
+
+		bval = &(BValue{
+			t:          LIST,
+			list_value: make([]BValue, length),
+		})
+
+		for index, item := range bval.list_value {
+			var item_bval *BValue
+
+			item_bval, err = NewBValue(item)
+
+			if err != nil {
+				return
+			}
+
+			bval.list_value[index] = *item_bval
+		}
+	default:
+		err = errors.New(fmt.Sprintf("Invalid type for bencoding: %v", t))
+	}
+
+	return
 }
 
-// func Bdecode(data []byte) BValue {
-// 	// 	
-// }
+func (bval BValue) String() string {
+	// Returns a JSON-like representation of this bencoding.Value.
 
-// func Encode() string {
-// 	val := Value{t: LIST, list_value: []Value{
-// 		Value{t: STRING, string_value: "Hello"},
-// 		Value{t: STRING, string_value: "World"}}}
+	switch bval.t {
+	case STRING:
+		return bval.string_value
 
-// 	return fmt.Sprintf("%v\n", val)
-// }
+	case INTEGER:
+		return fmt.Sprintf("%v", bval.int_value)
+
+	case LIST:
+		var buffer bytes.Buffer
+		buffer.WriteString("[")
+
+		for index, item := range bval.list_value {
+			if index > 0 {
+				buffer.WriteString(", ")
+			}
+
+			buffer.WriteString(item.String())
+		}
+
+		buffer.WriteString("]")
+		return buffer.String()
+
+	case DICTIONARY:
+		var buffer bytes.Buffer
+		buffer.WriteString("{")
+
+		first := true
+
+		for key, item := range bval.dict_value {
+			if first {
+				first = false
+			} else {
+				buffer.WriteString(", ")
+			}
+
+			buffer.WriteString(key)
+
+			buffer.WriteString(": ")
+
+			buffer.WriteString(item.String())
+		}
+
+		buffer.WriteString("}")
+		return buffer.String()
+	}
+
+	return fmt.Sprintf("<illegal BValue.t: %v>", bval.t)
+}
--- src/bitbucket.com/jeremybanks/go-distributed/main/main.go
+++ src/bitbucket.com/jeremybanks/go-distributed/main/main.go
@@ -7,18 +7,38 @@ import (
 
 func main() {
 	val, err := bencoding.NewBValue(map[string]interface{}{
-		"piece length": 1024,
+		"piece length": int64(1024),
 		"pieces":       "\x00234567890123456789\xFF",
 		"name":         "Test Data",
-		"length":       512,
+		"length":       int64(512),
 		"misc": map[string]interface{}{
 			"hello": "World!",
 		},
 	})
 
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-	} else {
-		fmt.Printf("Result: %v\n", val)
+		fmt.Printf("Error creating value:\n\t%v\n", err)
+		return
 	}
+
+	fmt.Printf("Value:\n\t%v\n\n", val)
+
+	bencoded, err := val.Bencode()
+
+	if err != nil {
+		fmt.Printf("Error encoding:\n\t%v\n", err)
+		return
+	}
+
+	fmt.Printf("Encoded:\n\t%v\n\n", bencoded)
+
+	restored, err := bencoding.Bdecode(bencoded)
+
+	if err != nil {
+		fmt.Printf("Error decoding:\n\t%v\n", err)
+		return
+	}
+
+	fmt.Printf("Decoded value:\n\t%v\n\n", restored)
+
 }
