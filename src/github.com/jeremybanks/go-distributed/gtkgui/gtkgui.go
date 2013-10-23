0e69706fed3d61a91780b055b083b900bc59baa6
Jeremy Banks <jeremy@jeremybanks.ca>
2013-10-22 21:27:49 -0400

    GUI tweaks

--- src/github.com/jeremybanks/go-distributed/gtkgui/gtkgui.go
+++ src/github.com/jeremybanks/go-distributed/gtkgui/gtkgui.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/jeremybanks/go-distributed/dht"
 	"github.com/mattn/go-gtk/gtk"
+	"math"
 	"os"
 	"time"
 )
@@ -13,7 +14,7 @@ const targetNoteCount = 32
 func Main() {
 	gtk.Init(nil)
 	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
-	window.SetTitle("distributedgtk")
+	window.SetTitle("Distributed-GTK")
 
 	// XXX: We should probably confirm that $HOME is not empty.
 	dhtClientStatePath := os.Getenv("HOME") + "/.distributed-dht.benc"
@@ -26,18 +27,44 @@ func Main() {
 	notebook := gtk.NewNotebook()
 	window.Add(notebook)
 
-	dhtPage := gtk.NewAlignment(0.1, 0.1, 0.1, 0.1)
+	dhtPage := MakeDhtVBox(dhtClient)
+	torrentPage := MakeTorrentVBox()
+
 	notebook.AppendPage(dhtPage, gtk.NewLabel("DHT"))
 
-	progress := gtk.NewProgressBar()
-	dhtPage.Add(progress)
+	notebook.AppendPage(torrentPage, gtk.NewLabel("Torrent"))
+
+	window.Connect("destroy", func() {
+		dhtClient.Save()
+		gtk.MainQuit()
+	})
+
+	window.SetSizeRequest(400, 500)
+	window.SetResizable(false)
+
+	window.ShowAll()
+
+	gtk.Main()
+}
+
+func MakeDhtVBox(dhtClient dht.Client) *gtk.VBox {
+	dhtPage := gtk.NewVBox(false, 0)
+
+	statusLabel := gtk.NewLabel("Connection Status")
+	statusLabel.SetAlignment(0.075, 1.0)
+	dhtPage.Add(statusLabel)
+
+	status := gtk.NewProgressBar()
+	statusAlign := gtk.NewAlignment(0.45, 0.0, 0.90, 0.1)
+	statusAlign.Add(status)
+	dhtPage.Add(statusAlign)
 
 	go func() {
 		for {
 			connectionInfo := dhtClient.ConnectionInfo()
-			progress.SetFraction(float64(connectionInfo.GoodNodes) / targetNoteCount)
-			progress.SetText(fmt.Sprintf(
-				"%v Good Known Nodes\n(%v unknown, %v bad)",
+			status.SetFraction(math.Min(float64(connectionInfo.GoodNodes)/targetNoteCount, 1.0))
+			status.SetText(fmt.Sprintf(
+				"%v Good Nodes\n(%v unknown, %v bad)",
 				connectionInfo.GoodNodes,
 				connectionInfo.UnknownNodes,
 				connectionInfo.BadNodes,
@@ -46,13 +73,13 @@ func Main() {
 		}
 	}()
 
-	window.Connect("destroy", func() {
-		dhtClient.Save()
-		gtk.MainQuit()
-	})
+	return dhtPage
+}
 
-	window.ShowAll()
-	window.SetSizeRequest(600, 400)
+func MakeTorrentVBox() *gtk.VBox {
+	dhtPage := gtk.NewVBox(false, 0)
 
-	gtk.Main()
+	dhtPage.Add(gtk.NewLabel("NotImplemented :("))
+
+	return dhtPage
 }
